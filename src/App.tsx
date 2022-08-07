6dc5e8247e52cc0e4fcda66e56d6a97d41932426
Minyoung Lee <leemin02_@naver.com>
2022-08-08 01:00:36 +0900

    feat: add Auth, TodoList pages in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Auth, TodoList } from './pages';
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        TEST
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
