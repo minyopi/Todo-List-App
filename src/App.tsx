888170e06a82fb7beb3c8bf6c7628a7795a45d36
Minyoung Lee <leemin02_@naver.com>
2022-08-08 01:13:33 +0900

    style: add reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/global/Header';
 import { Auth, TodoList } from './pages';
+import './styles/reset.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        TEST
+        <Header />
         <Routes>
           <Route path="/" element={<TodoList />} />
           <Route path="/auth" element={<Auth />} />
