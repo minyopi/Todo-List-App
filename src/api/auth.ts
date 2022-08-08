2b885af8ec2edd547b2d4e411ee0752b94be0247
Minyoung Lee <leemin02_@naver.com>
2022-08-08 16:08:23 +0900

    feat(Auth): change login api logic

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 axios.defaults.baseURL = 'http://localhost:8080';
 
@@ -8,17 +7,9 @@ interface AuthProps {
   password: string;
 }
 
-export const login = async (authInfo: AuthProps) => {
-  const res = await axios.post('/user/login', authInfo);
-  if (res.status === 200) {
-    Cookies.set('token', res.data.token);
-    return '회원가입에 성공했습니다';
-  }
-  if (res.status === 409) {
-    return '이미 존재하는 유저 입니다';
-  }
-
-  return '잘못된 요청입니다.';
+export const login = (authInfo: AuthProps) => {
+  const res = axios.post('/users/login', authInfo);
+  return res;
 };
 
 export const signUp = (authInfo: AuthProps) => {
