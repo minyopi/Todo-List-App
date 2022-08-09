8d0ba279ef0d1ac06cdb9f3b83a826f31523a82d
Minyoung Lee <leemin02_@naver.com>
2022-08-09 16:24:03 +0900

    feat(TodoList): add get todos and create todos feature

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,5 +1,116 @@
+import styled from 'styled-components';
+import StyledLink from '../styles/common/StyledLink';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMount } from 'react-use';
+import { createTodo, getTodo } from '../api/todoList';
+import { getTodoResponse } from '../typings/todoList';
+
+const StyledTodoListWrapper = styled.div`
+  padding: 40px;
+
+  h1 {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
 const TodoList: React.FC = () => {
-  return <div>todolist</div>;
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem('token');
+  const [todos, setTodos] = useState<getTodoResponse>({ data: [] });
+  const [formValue, setFormValue] = useState({ title: '', content: '' });
+
+  console.log(todos);
+
+  const getTodos = async () => {
+    const response = await getTodo();
+    setTodos(response.data);
+  };
+
+  useMount(async () => {
+    getTodos();
+  });
+
+  useEffect(() => {
+    if (!authToken) {
+      alert('로그인이 필요합니다!');
+      navigate('/auth');
+    }
+  }, [authToken]);
+
+  const renderCreateTodoForm = () => {
+    return (
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            await createTodo(formValue);
+            await getTodos();
+            setFormValue({ title: '', content: '' });
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        {/* Title */}
+        <div>
+          title:
+          <input
+            type="text"
+            value={formValue.title}
+            onChange={(e) => {
+              setFormValue((prev) => {
+                return { ...prev, title: e.target.value };
+              });
+            }}
+          />
+        </div>
+
+        {/* Content */}
+        <div>
+          content:
+          <textarea
+            value={formValue.content}
+            onChange={(e) => {
+              setFormValue((prev) => {
+                return { ...prev, content: e.target.value };
+              });
+            }}
+          />
+        </div>
+
+        {/* Add Button */}
+        <button type="submit">추가하기</button>
+      </form>
+    );
+  };
+
+  const renderTodoList = () => {
+    if (todos.data.length === 0) {
+      return;
+    }
+
+    const todoList = todos.data.map((todo) => {
+      return (
+        <li>
+          <StyledLink to={`/detail/${todo.id}`}>{`- ${todo.title}`}</StyledLink>
+          <button>수정</button>
+          <button>삭제</button>
+        </li>
+      );
+    });
+
+    return todoList;
+  };
+
+  return (
+    <StyledTodoListWrapper>
+      <h1>Todo List</h1>
+      <div>{renderCreateTodoForm()}</div>
+      <ul>{renderTodoList()}</ul>
+    </StyledTodoListWrapper>
+  );
 };
 
 export default TodoList;
