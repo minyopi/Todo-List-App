499158365bb08ce09554404b41d7427e9e4c583d
Minyoung Lee <leemin02_@naver.com>
2022-08-09 16:24:27 +0900

    feat: add TodoListDetail page

---
+++ src/pages/TodoListDetail.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMount } from 'react-use';
+import styled from 'styled-components';
+import { getTodoById } from '../api/todoList';
+import { todoData } from '../typings/todoList';
+
+const StyledTodoDetailWrapper = styled.div`
+  padding: 40px;
+`;
+
+const StyledH1 = styled.h1`
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 20px;
+`;
+
+const TodoListDetail: React.FC = () => {
+  const { id } = useParams();
+
+  const [todoDetail, setTodoDetail] = useState<todoData>();
+
+  useMount(async () => {
+    if (!id) {
+      return;
+    }
+
+    const response = await getTodoById(id);
+    setTodoDetail(response.data.data);
+  });
+
+  return (
+    <>
+      {todoDetail ? (
+        <StyledTodoDetailWrapper>
+          <StyledH1>Detail Page</StyledH1>
+          <div>title: {todoDetail.title}</div>
+          <div>content: {todoDetail.content}</div>
+          <div>createdAt: {todoDetail.createdAt}</div>
+          <div>updateAt: {todoDetail.updatedAt}</div>
+        </StyledTodoDetailWrapper>
+      ) : (
+        <div>정보를 불러올 수 없습니다.</div>
+      )}
+    </>
+  );
+};
+
+export default TodoListDetail;
