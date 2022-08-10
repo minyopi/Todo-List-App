7bf862bb0931001104699ad6123163dc444d8701
Minyoung Lee <leemin02_@naver.com>
2022-08-11 00:52:47 +0900

    edit: change App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 import AppRoutes from './App.routes';
 import Header from './components/global/Header';
 
-import GlobalStyle from './styles/GlobalStyles';
 import './styles/reset.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <RecoilRoot>
-        <div className="App">
-          <Header />
-          <AppRoutes />
-        </div>
-      </RecoilRoot>
-    </BrowserRouter>
+    <div className="App">
+      <Header />
+      <AppRoutes />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from './styles/GlobalStyles';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>,
 );
-root.render(<App />);
