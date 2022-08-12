22ce0bfb2ae41479d8dd41e3648faf75bb3ce960
Minyoung Lee <leemin02_@naver.com>
2022-08-13 00:02:24 +0900

    edit: refactor edit: refactor abstract #1 - (4)
    - 이슈 #1 (4) 적절히 추상화 되지 않은 함수와 컴포넌트
    - renderTodoList의 return 값이 추상화가 되어있지 않아서 한눈에 파악하기 힘들다 생각하여 각 내용들을 추상화

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -20,7 +20,9 @@ const StyledTodoListWrapper = styled.div`
 
 const TodoList: React.FC = () => {
   const { token } = useRecoilValue(authState);
-  const { control, handleSubmit, setValue } = useForm();
+  const createForm = useForm();
+  const editForm = useForm();
+
   const [nowEditMode, setNowEditMode] = useState(false);
   const [nowClicked, setNowClicked] = useState(0);
   const [todos, setTodos] = useState<{ data: TodoData[] }>();
@@ -36,34 +38,36 @@ const TodoList: React.FC = () => {
 
   const renderCreateTodoForm = () => {
     const onSubmit: SubmitHandler<FieldValues> = async (values) => {
-      const formValue = { title: values.createTitle, content: values.createContent };
+      const formValue = { title: values.title, content: values.content };
 
       try {
         await createTodo(formValue, token);
         await getTodos(token);
-        setValue('createTitle', '');
-        setValue('createContent', '');
+        createForm.setValue('title', '');
+        createForm.setValue('content', '');
       } catch (error) {
         console.error(error);
       }
     };
 
     return (
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={createForm.handleSubmit(onSubmit)}>
         <div>
           title:
           <Controller
-            control={control}
-            name="createTitle"
+            control={createForm.control}
+            name="title"
             render={({ field }) => <input {...field} defaultValue="" placeholder="할일을 입력해주세요." />}
+            rules={{ required: true }}
           />
         </div>
         <div>
           content:
           <Controller
-            control={control}
-            name="createContent"
+            control={createForm.control}
+            name="content"
             render={({ field }) => <textarea {...field} defaultValue="" placeholder="자세한 내용을 입력해주세요." />}
+            rules={{ required: true }}
           />
         </div>
         <button type="submit">추가하기</button>
@@ -76,9 +80,9 @@ const TodoList: React.FC = () => {
       return;
     }
 
-    const todoList = todos.data.map((todo, idx) => {
+    const showEditForm = (todo: TodoData) => {
       const onSubmit: SubmitHandler<FieldValues> = async (values) => {
-        const editValue = { title: values.editTitle, content: values.editContent };
+        const editValue = { title: values.title, content: values.content };
 
         await updateTodoList(todo.id, editValue, token);
         await getTodos(token);
@@ -86,66 +90,67 @@ const TodoList: React.FC = () => {
       };
 
       return (
-        <li key={todo.id}>
-          {nowEditMode && nowClicked === idx ? (
-            <>
-              {/* Edit Mode */}
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  title:
-                  <Controller
-                    control={control}
-                    name="editTitle"
-                    render={({ field }) => (
-                      <input {...field} defaultValue={todo.title} placeholder="할일을 입력해주세요." />
-                    )}
-                  />
-                </div>
-                <div>
-                  content:
-                  <Controller
-                    control={control}
-                    name="editContent"
-                    render={({ field }) => (
-                      <textarea {...field} defaultValue={todo.content} placeholder="자세한 내용을 입력해주세요." />
-                    )}
-                  />
-                </div>
-                <button type="submit">수정</button>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setNowEditMode(false);
-                  }}
-                >
-                  취소
-                </button>
-              </form>
-            </>
-          ) : (
-            <>
-              {/* Todo List */}
-              <StyledLink to={`/detail/${todo.id}`}>{`- ${todo.title}`}</StyledLink>
-              <button
-                onClick={() => {
-                  setNowEditMode(true);
-                  setNowClicked(idx);
-                }}
-              >
-                수정
-              </button>
-              <button
-                onClick={async () => {
-                  await deleteTodoList(todo.id, token);
-                  await getTodos(token);
-                }}
-              >
-                삭제
-              </button>
-            </>
-          )}
-        </li>
+        <form onSubmit={editForm.handleSubmit(onSubmit)}>
+          <div>
+            title:
+            <Controller
+              control={editForm.control}
+              name="title"
+              render={({ field }) => <input {...field} defaultValue={todo.title} placeholder="할일을 입력해주세요." />}
+              rules={{ required: true }}
+            />
+          </div>
+          <div>
+            content:
+            <Controller
+              control={editForm.control}
+              name="content"
+              render={({ field }) => (
+                <textarea {...field} defaultValue={todo.content} placeholder="자세한 내용을 입력해주세요." />
+              )}
+              rules={{ required: true }}
+            />
+          </div>
+          <button type="submit">수정</button>
+          <button
+            type="button"
+            onClick={() => {
+              setNowEditMode(false);
+            }}
+          >
+            취소
+          </button>
+        </form>
       );
+    };
+
+    const showTodoList = (todo: TodoData, idx: number) => {
+      return (
+        <>
+          {/* Todo List */}
+          <StyledLink to={`/detail/${todo.id}`}>{`- ${todo.title}`}</StyledLink>
+          <button
+            onClick={() => {
+              setNowEditMode(true);
+              setNowClicked(idx);
+            }}
+          >
+            수정
+          </button>
+          <button
+            onClick={async () => {
+              await deleteTodoList(todo.id, token);
+              await getTodos(token);
+            }}
+          >
+            삭제
+          </button>
+        </>
+      );
+    };
+
+    const todoList = todos.data.map((todo, idx) => {
+      return <li key={todo.id}>{nowEditMode && nowClicked === idx ? showEditForm(todo) : showTodoList(todo, idx)}</li>;
     });
 
     return todoList;
