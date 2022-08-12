41ded6e87f9321fba26c9968c4956d017b4c6d37
Minyoung Lee <leemin02_@naver.com>
2022-08-12 20:28:49 +0900

    edit: edit App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import { authState } from './store/auth';
 import { setAxiosConfig } from './api';
@@ -5,19 +7,19 @@ import AppRoutes from './App.routes';
 import Header from './components/global/Header';
 
 import './styles/reset.css';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const setAuthToken = useSetRecoilState(authState);
   const navigate = useNavigate();
+  const setAuthToken = useSetRecoilState(authState);
+
   const token = localStorage.getItem('token');
 
   useEffect(() => {
     if (!token || typeof token !== 'string') {
       setAuthToken({ token: undefined });
-      navigate('/auth');
       alert('로그인이 필요합니다');
+      navigate('/auth');
+
       return;
     }
 
