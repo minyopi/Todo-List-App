b243c8c10da282ca7d07f42b540de2085c16d7ee
Minyoung Lee <leemin02_@naver.com>
2022-08-13 10:59:41 +0900

    feat: make auth utils condition strict

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,15 @@
 export const isEmailValid = (email: string) => {
+  if (!email || typeof email !== 'string') {
+    return false;
+  }
+
   return email.includes('@') && email.includes('.');
 };
 
 export const isPasswordValid = (password: string) => {
+  if (!password || typeof password !== 'string') {
+    return false;
+  }
+
   return password.length >= 8;
 };
