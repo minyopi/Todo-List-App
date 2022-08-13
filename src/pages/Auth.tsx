2ad080a1df64afd4b0d7d9bfa4703066b8461214
Minyoung Lee <leemin02_@naver.com>
2022-08-13 11:00:18 +0900

    feat(Auth): use useForm in auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,32 +4,35 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { postLogin, postSignUp } from '../api/auth';
 import { authState } from '../store/auth';
-import { authProps } from '../typings/auth';
 import { isEmailValid, isPasswordValid } from '../utils/auth';
+import { Controller, useForm } from 'react-hook-form';
+
+import type { FieldValues, SubmitHandler } from 'react-hook-form';
 
 const StyledButton = styled.button<{ isValid: boolean }>`
   cursor: ${({ isValid }) => (isValid ? 'pointer' : 'not-allowed')};
 `;
 
 const Auth: React.FC = () => {
+  const loginForm = useForm();
+  const signupForm = useForm();
   const navigate = useNavigate();
-  const [authInfo, setAuthInfo] = useRecoilState(authState);
 
-  const [loginValue, setLoginValue] = useState<authProps>({ email: '', password: '' });
-  const [signUpValue, setSignUpValue] = useState<authProps>({ email: '', password: '' });
+  const [authInfo, setAuthInfo] = useRecoilState(authState);
   const [isLoginValid, setIsLoginValid] = useState(false);
   const [isSignUpValid, setIsSignUpValid] = useState(false);
 
-  const isLoginInfoValid = isEmailValid(loginValue.email) && isPasswordValid(loginValue.password);
-  const isSignUpInfoValid = isEmailValid(signUpValue.email) && isPasswordValid(signUpValue.password);
+  const isLoginInfoValid = isEmailValid(loginForm.getValues().email) && isPasswordValid(loginForm.getValues().password);
+  const isSignUpInfoValid =
+    isEmailValid(signupForm.getValues().email) && isPasswordValid(signupForm.getValues().password);
 
   useEffect(() => {
     isLoginInfoValid ? setIsLoginValid(true) : setIsLoginValid(false);
     isSignUpInfoValid ? setIsSignUpValid(true) : setIsSignUpValid(false);
-  }, [loginValue, signUpValue]);
+  }, [loginForm, signupForm]);
 
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleLogin: SubmitHandler<FieldValues> = async (values) => {
+    const loginValue = { email: values.email, password: values.password };
 
     if (authInfo.token) {
       navigate('/');
@@ -49,8 +52,8 @@ const Auth: React.FC = () => {
     }
   };
 
-  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSignUp: SubmitHandler<FieldValues> = async (values) => {
+    const signUpValue = { email: values.email, password: values.password };
 
     try {
       await postSignUp(signUpValue).then((res) => {
@@ -65,27 +68,21 @@ const Auth: React.FC = () => {
 
   const renderLogin = () => {
     return (
-      <form onSubmit={handleLogin}>
+      <form onSubmit={loginForm.handleSubmit(handleLogin)}>
         <h2>로그인</h2>
-        <input
-          type="text"
-          value={loginValue.email}
-          placeholder="email"
-          onChange={(e) => {
-            setLoginValue((prev) => {
-              return { ...prev, email: e.target.value };
-            });
-          }}
+        <Controller
+          control={loginForm.control}
+          name="email"
+          render={({ field }) => <input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
+          rules={{ required: true }}
         />
-        <input
-          type="password"
-          value={loginValue.password}
-          placeholder="password"
-          onChange={(e) => {
-            setLoginValue((prev) => {
-              return { ...prev, password: e.target.value };
-            });
-          }}
+        <Controller
+          control={loginForm.control}
+          name="password"
+          render={({ field }) => (
+            <input {...field} type="password" defaultValue="" placeholder="이메일을 입력해주세요" />
+          )}
+          rules={{ required: true }}
         />
         <StyledButton type="submit" disabled={!isLoginValid} isValid={isLoginValid}>
           로그인
@@ -96,31 +93,21 @@ const Auth: React.FC = () => {
 
   const renderSignUp = () => {
     return (
-      <form
-        onSubmit={(e) => {
-          handleSignUp(e);
-        }}
-      >
+      <form onSubmit={signupForm.handleSubmit(handleSignUp)}>
         <h2>회원가입</h2>
-        <input
-          type="text"
-          value={signUpValue.email}
-          placeholder="email"
-          onChange={(e) => {
-            setSignUpValue((prev) => {
-              return { ...prev, email: e.target.value };
-            });
-          }}
+        <Controller
+          control={signupForm.control}
+          name="email"
+          render={({ field }) => <input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
+          rules={{ required: true }}
         />
-        <input
-          type="password"
-          value={signUpValue.password}
-          placeholder="password"
-          onChange={(e) => {
-            setSignUpValue((prev) => {
-              return { ...prev, password: e.target.value };
-            });
-          }}
+        <Controller
+          control={signupForm.control}
+          name="password"
+          render={({ field }) => (
+            <input {...field} type="password" defaultValue="" placeholder="이메일을 입력해주세요" />
+          )}
+          rules={{ required: true }}
         />
         <StyledButton type="submit" disabled={!isSignUpValid} isValid={isSignUpValid}>
           회원 가입
