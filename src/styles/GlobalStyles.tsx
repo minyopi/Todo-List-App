26a255e43dbd2c65a8ca8b649cfe09de12dc8c26
Minyoung Lee <leemin02_@naver.com>
2022-08-15 12:59:53 +0900

    fix: fix lint error of styled-reset

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+import { reset } from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+
     * {
         color: #3a3a3a;
         font-size: 16px;
