610dbdab04c33719d9bd405f9d98b6a8bedc2516
Minyoung Lee <leemin02_@naver.com>
2022-08-15 14:09:21 +0900

    style: apply ant design for form

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,17 +1,12 @@
-import styled from 'styled-components';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { postLogin, postSignUp } from '../api/auth';
 import { authState } from '../store/auth';
 import { isEmailValid, isPasswordValid } from '../utils/auth';
 import { Controller, useForm } from 'react-hook-form';
+import { Button, Form, Input } from 'antd';
 
 import type { FieldValues, SubmitHandler } from 'react-hook-form';
-
-const StyledButton = styled.button<{ isValid: boolean }>`
-  cursor: ${({ isValid }) => (isValid ? 'pointer' : 'not-allowed')};
-`;
 
 const Auth: React.FC = () => {
   const loginForm = useForm();
@@ -19,17 +14,6 @@ const Auth: React.FC = () => {
   const navigate = useNavigate();
 
   const [authInfo, setAuthInfo] = useRecoilState(authState);
-  const [isLoginValid, setIsLoginValid] = useState(false);
-  const [isSignUpValid, setIsSignUpValid] = useState(false);
-
-  const isLoginInfoValid = isEmailValid(loginForm.getValues().email) && isPasswordValid(loginForm.getValues().password);
-  const isSignUpInfoValid =
-    isEmailValid(signupForm.getValues().email) && isPasswordValid(signupForm.getValues().password);
-
-  useEffect(() => {
-    isLoginInfoValid ? setIsLoginValid(true) : setIsLoginValid(false);
-    isSignUpInfoValid ? setIsSignUpValid(true) : setIsSignUpValid(false);
-  }, [loginForm, signupForm]);
 
   const handleLogin: SubmitHandler<FieldValues> = async (values) => {
     const loginValue = { email: values.email, password: values.password };
@@ -43,6 +27,7 @@ const Auth: React.FC = () => {
       await postLogin(loginValue).then((res) => {
         localStorage.setItem('token', res.data.token);
         setAuthInfo({ token: res.data.token });
+        navigate('/');
       });
 
       navigate('/');
@@ -68,51 +53,75 @@ const Auth: React.FC = () => {
 
   const renderLogin = () => {
     return (
-      <form onSubmit={loginForm.handleSubmit(handleLogin)}>
+      <Form
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        autoComplete="off"
+        onFinish={loginForm.handleSubmit(handleLogin)}
+      >
         <h2>로그인</h2>
-        <Controller
-          control={loginForm.control}
-          name="email"
-          render={({ field }) => <input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
-          rules={{ required: true }}
-        />
-        <Controller
-          control={loginForm.control}
-          name="password"
-          render={({ field }) => (
-            <input {...field} type="password" defaultValue="" placeholder="이메일을 입력해주세요" />
-          )}
-          rules={{ required: true }}
-        />
-        <StyledButton type="submit" disabled={!isLoginValid} isValid={isLoginValid}>
-          로그인
-        </StyledButton>
-      </form>
+        <Form.Item label="Email" name="email">
+          <Controller
+            control={loginForm.control}
+            name="email"
+            render={({ field }) => <Input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
+            rules={{ required: true, validate: (value) => isEmailValid(value) }}
+          />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Controller
+            control={loginForm.control}
+            name="password"
+            render={({ field }) => (
+              <Input.Password {...field} type="password" defaultValue="" placeholder="비밀번호를 입력해주세요" />
+            )}
+            rules={{ required: true, validate: (value) => isPasswordValid(value) }}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            로그인
+          </Button>
+        </Form.Item>
+      </Form>
     );
   };
 
   const renderSignUp = () => {
     return (
-      <form onSubmit={signupForm.handleSubmit(handleSignUp)}>
+      <Form
+        name="signUp"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        autoComplete="off"
+        onFinish={signupForm.handleSubmit(handleSignUp)}
+      >
         <h2>회원가입</h2>
-        <Controller
-          control={signupForm.control}
-          name="email"
-          render={({ field }) => <input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
-          rules={{ required: true }}
-        />
-        <Controller
-          control={signupForm.control}
-          name="password"
-          render={({ field }) => (
-            <input {...field} type="password" defaultValue="" placeholder="이메일을 입력해주세요" />
-          )}
-          rules={{ required: true }}
-        />
-        <StyledButton type="submit" disabled={!isSignUpValid} isValid={isSignUpValid}>
-          회원 가입
-        </StyledButton>
-      </form>
+        <Form.Item label="Email" name="email">
+          <Controller
+            control={signupForm.control}
+            name="email"
+            render={({ field }) => <Input {...field} type="text" defaultValue="" placeholder="이메일을 입력해주세요" />}
+            rules={{ required: true, validate: (value) => isEmailValid(value) }}
+          />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Controller
+            control={signupForm.control}
+            name="password"
+            render={({ field }) => (
+              <Input.Password {...field} type="password" defaultValue="" placeholder="비밀번호를 입력해주세요" />
+            )}
+            rules={{ required: true, validate: (value) => isPasswordValid(value) }}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            회원 가입
+          </Button>
+        </Form.Item>
+      </Form>
     );
   };
 
